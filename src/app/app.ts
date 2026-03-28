136f53046ff94c6a59124a513b3973b2bea2cccd
sudipta2210 <69044137+sudipta2210@users.noreply.github.com>
2026-03-28 21:33:27 +0530

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { COURSES } from '../course.data';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('day2');
+
+  //2 Way Data Binding  & If-else
+  public name:string="";
+  public flag:boolean=false;
+
+  public COURSELST:any=COURSES;
+  FnKeyup()
+  {
+    console.log(this.name);
+    if(this.name=="snigdha")
+      this.flag=true;
+    else 
+      this.flag=false;
+  }
+
+  //ends here//
+
+
 }
