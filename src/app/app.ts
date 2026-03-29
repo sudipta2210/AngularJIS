33eeae956fa4f6a4e0f42055ebb909f249ac2016
sudipta2210 <69044137+sudipta2210@users.noreply.github.com>
2026-03-29 13:11:02 +0530

    init commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { COURSES } from '../course.data';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit,OnDestroy {
   protected readonly title = signal('courseproj');
+  public courseList:any;
+  //Life Cycle Hooks
+  constructor(){
+    console.log("Constructor is called");
+    this.courseList=[];
+  }
+
+  ngOnInit(): void {
+    console.log("ngOninit called");
+    this.courseList=COURSES;
+  }
+
+  ngOnDestroy(): void {
+    console.log("ngOndestroy Called");
+  }
 }
